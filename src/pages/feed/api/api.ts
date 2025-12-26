254ea53043b7a5dc061fe2b7cbd45999aceaa7bd
JeongSH1 <jyt001532@gmail.com>
2025-12-26 23:06:33 +0900

    fix: 서버 호스트 수정

--- src/pages/feed/api/api.ts
+++ src/pages/feed/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const feedApi = axios.create({
-    // baseURL: "https://mcphubcorp.site/",
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://mcphubcorp.site/",
+    //baseURL: "http://localhost:8080/",
     timeout: 5000,
 });
